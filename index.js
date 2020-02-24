af397589450261f1ec1c70c106f6c8ed930b6ab8
Kacy Sommers <kacysommers@Kacys-MacBook-Pro.local>
2020-02-24 09:00:05 -0800

    undoing any progress ive made

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
+require('./database');
 const express = require('express');
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 const app = express();
 const cloudinary = require('cloudinary');
-const db = require('./database');
 const Image = require('./models/image.model');
 const bp = require('body-parser');
 
@@ -24,69 +24,10 @@ app.use(cors());
 app.use(bp.json());
 
 app.post('/photos', (req, res) => {
-  const { filters, limit, offset, includeTotal } = req.body;
+  console.log('Hit photos route');
+  const { filters, limit, offsets } = req.body;
   if (filters && filters.length) {
     const fields = filters.map(tag => ({ tag }));
-    console.log('Fields::: ', fields);
-    const count = () => {
-      return new Promise((resolve, reject) => {
-        Image.count({ $or: fields }, (err, total) => {
-          if (!err) {
-            console.log('TOTLA:::: ', total)
-            resolve(total);
-          } else {
-            reject(err);
-          }
-        });
-      });
-    }
-
-    const find = (response, total) => {
-      Image.find({ $or: fields }, (err, images) => {
-        if (err) {
-          response.sendStatus(500).json(err);
-        } else {
-          console.log('new photos offset', offset)
-          response.json({ images, total });
-        }
-      }).skip(offset).limit(limit);
-    }
-
-    if (includeTotal) {
-      count()
-        .then(total => find(res, total))
-        .catch(err => res.sendStatus(500).json(err))
-    } else {
-      find(res);
-    }
-
-    // const expression = tags.reduce((ex, tag, i) => {
-    //   let newStr = i === 0 ? ' (' : ' OR ';
-    //   newStr += `tags:${tag}`;
-    //   if (i === tags.length - 1) {
-    //     newStr += ')';
-    //   }
-    //   return ex + newStr;
-    // }, '');
-    // cloudinary.v2.search
-    //   .expression(`resource_type:image AND folder:"2K Homecare/*" AND${expression}`)
-    //   .execute()
-    //   .then(result => {
-    //     const imgMap = {};
-    //     if (result && result.resources) {
-    //       result.resources.forEach(img => {
-    //         const filter = parseFolder(img.folder);
-
-    //         if (imgMap[filter]) {
-    //           imgMap[filter].push(img.url);
-    //         } else {
-    //           imgMap[filter] = [img.url];
-    //         }
-    //       });
-    //     }
-    //     res.json(imgMap);
-    //   })
-    //   .catch(err => res.sendStatus(500).json(err));
   } else {
     res.sendStatus(400).json({ error: 'No filters in query' })
   }
