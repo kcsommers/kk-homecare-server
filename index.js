aa1030abfd361190073263f8c7896e972561e371
Kacy Sommers <kacy@dev.adaptiva.com>
2020-02-24 17:35:54 -0800

    removing tag nonsense

--- index.js
+++ index.js
@@ -4,32 +4,44 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 const app = express();
-const cloudinary = require('cloudinary');
 const Image = require('./models/image.model');
 const bp = require('body-parser');
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_SECRET
-// });
 
 const parseFolder = folder => {
   // 2K Homecare/painting
   const segments = folder.split('/');
   return segments[segments.length - 1];
-}
+};
 
 app.use(cors());
 app.use(bp.json());
 
 app.post('/photos', (req, res) => {
   console.log('Hit photos route');
-  const { filters, limit, offsets } = req.body;
-  if (filters && filters.length) {
-    const fields = filters.map(tag => ({ tag }));
+  const { limit, lastId, getTotal, fetchAll } = req.body;
+  const handleResponse = (error, images) => {
+    if (error) {
+      res.sendStatus(500).json({ error });
+    } else {
+      res.json({ images });
+    }
+  }
+  const fetchImages = () => {
+    if (fetchAll) {
+      return Image.find();
+    }
+    if (lastId) {
+      return Image.find({ _id: { $gt: lastId } }).limit(limit);
+    } else {
+      return Image.find().limit(limit);
+    }
+  }
+  if (getTotal) {
+    Image.countDocuments().exec().then((total) => {
+      fetchImages().exec(handleResponse)
+    });
   } else {
-    res.sendStatus(400).json({ error: 'No filters in query' })
+    fetchImages().exec(handleResponse);
   }
 });
 
